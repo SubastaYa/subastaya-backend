0547311e8568783e60a7a6a2128fdb8236e72226
Javier Florentin <javier.florentin.pri@gmail.com>
2026-08-29 19:17:08 -0300

    feat: agregar entidad auditlog

---
+++ SubastaYa.Core/Entities/AuditLog.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubastaYa.Core.Entities
+{
+    public class AuditLog
+    {
+        public Guid Id { get; private set; }
+        public string Accion { get; private set; }
+        public string Detalles { get; private set; }
+        public string? EntidadAfectada { get; private set; }
+        public string? EntidadId { get; private set; }
+        public Guid? UsuarioId { get; private set; }
+        public DateTime FechaEvento { get; private set; }
+
+        public AuditLog(string accion, string detalles, string? entidadAfectada = null, string? entidadId = null, Guid? usuarioId = null)
+        {
+            Id = Guid.NewGuid();
+            Accion = accion;
+            Detalles = detalles;
+            EntidadAfectada = entidadAfectada;
+            EntidadId = entidadId;
+            UsuarioId = usuarioId;
+            FechaEvento = DateTime.UtcNow;
+        }
+
+        protected AuditLog() { }
+    }
+}
--- SubastaYa.Infrastructure/Data/ApplicationDbContext.cs
+++ SubastaYa.Infrastructure/Data/ApplicationDbContext.cs
@@ -1,12 +1,23 @@
 using Microsoft.EntityFrameworkCore;
+using SubastaYa.Core.Entities;
 
 namespace SubastaYa.Infrastructure.Data
 {
     public class ApplicationDbContext : DbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options){}
+
+        public DbSet<AuditLog> AuditLogs { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            base.OnModelCreating(modelBuilder);
+                        
+            modelBuilder.Entity<AuditLog>()
+                .ToTable(tb => tb.UseSqlOutputClause(false));
+
+            modelBuilder.Entity<AuditLog>().Property(a => a.Accion).IsRequired().HasMaxLength(100);
+            modelBuilder.Entity<AuditLog>().Property(a => a.Detalles).IsRequired();
         }
     }
 }
