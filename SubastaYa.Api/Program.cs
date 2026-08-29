f9862634b679f8eed62fa8f7d00dd0eb9312397e
Javier Florentin <javier.florentin.pri@gmail.com>
2026-08-29 13:57:13 -0300

    build: agrego middleware en program

--- SubastaYa.Api/Program.cs
+++ SubastaYa.Api/Program.cs
@@ -1,17 +1,31 @@
+using SubastaYa.Api.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
 
+
+app.UseMiddleware<ExceptionMiddleware>();
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
 app.UseHttpsRedirection();
 
 app.Run();
