fdd4a5174eaa5bb6db2714fc78ce9b7e10c0d824
Julian-Antunes <julian_0007@hotmail.com>
2026-09-13 18:38:45 -0300

    feat: exponer endpoint de categorias para catalogo y publicacion

--- Presentation/Controllers/CategoriasController.cs
+++ Presentation/Controllers/CategoriasController.cs
@@ -6,13 +6,17 @@ namespace Presentation.Controllers
 {
     [ApiController]
     [Route("api/categorias")]
+    [Route("api/categories")]
     public class CategoriasController : ControllerBase
     {
         private readonly IQueryHandler<ObtenerCategoriasQuery, IReadOnlyList<CategoriaDto>> _categoriasHandler;
+
         public CategoriasController(IQueryHandler<ObtenerCategoriasQuery, IReadOnlyList<CategoriaDto>> categoriasHandler)
         {
             _categoriasHandler = categoriasHandler;
         }
+
+        // Endpoint público para cargar dinámicamente las categorías en el catálogo y formulario de subastas
         [HttpGet]
         public async Task<IActionResult> GetAll(CancellationToken ct)
         {
